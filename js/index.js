a6d61e74db8954978821df8518cef45a66e51e4f
salmanpatnee <salmanpatni92@gmail.com>
2024-09-08 11:42:13 +0500

    Milestone 3 generate resume with user input.

--- js/index.js
+++ js/index.js
@@ -10,3 +10,164 @@ document.addEventListener('DOMContentLoaded', function () {
         button.textContent = isOpen ? 'Show Less' : 'Show More Experience';
     });
 });
+var fullname = document.getElementById("fullname");
+var pFullname = document.getElementById("p_fullname");
+fullname.addEventListener("blur", function (event) {
+    var value = event.target.value;
+    if (!value.trim()) {
+        // Show an alert if the input is empty
+        alert("Full name cannot be empty!");
+        pFullname.textContent = "Please enter your full name";
+    }
+    else {
+        // Update the p tag if the input is not empty
+        pFullname.textContent = value;
+    }
+    // pFullname.textContent = value;
+});
+var tagline = document.getElementById("tagline");
+var pTagline = document.getElementById("p_tagline");
+tagline.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pTagline.textContent = value;
+});
+var bio = document.getElementById("bio");
+var pbio = document.getElementById("p_bio");
+bio.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pbio.textContent = value;
+});
+var phone = document.getElementById("phone");
+var pPhone = document.getElementById("p_phone");
+phone.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pPhone.textContent = value;
+    pPhone.href = "tel:".concat(value);
+});
+var email = document.getElementById("email");
+var pEmail = document.getElementById("p_email");
+email.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pEmail.textContent = value;
+    pEmail.href = "mailto:".concat(value);
+});
+var github = document.getElementById("github_url");
+var pGithub = document.getElementById("p_github");
+github.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pGithub.textContent = value;
+    pGithub.href = "".concat(value);
+});
+var linkedin = document.getElementById("linkedin_url");
+var pLinkedin = document.getElementById("p_linkedin");
+linkedin.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pLinkedin.textContent = value;
+    pLinkedin.href = "".concat(value);
+});
+var education_1 = document.getElementById("education_1");
+var pEducation_1 = document.getElementById("p_education_title_1");
+education_1.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pEducation_1.textContent = value;
+});
+var education_1_year = document.getElementById("education_1_year");
+var pEducation_1_year = document.getElementById("p_education_year_1");
+education_1_year.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pEducation_1_year.textContent = value;
+});
+var education_2 = document.getElementById("education_2");
+var pEducation_2 = document.getElementById("p_education_title_2");
+education_2.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pEducation_2.textContent = value;
+});
+var education_2_year = document.getElementById("education_2_year");
+var pEducation_2_year = document.getElementById("p_education_year_2");
+education_2_year.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pEducation_2_year.textContent = value;
+});
+var skills = document.getElementById("skills");
+var pSkills = document.getElementById("p_skills");
+skills.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pSkills.innerHTML = '';
+    var items = value.split(',').map(function (item) { return item.trim(); });
+    items.forEach(function (item) {
+        if (item) { // Ensure the item is not an empty string
+            var li = document.createElement('li');
+            li.textContent = item;
+            pSkills.appendChild(li);
+        }
+    });
+});
+var languages = document.getElementById("languages");
+var pLanguages = document.getElementById("p_languages");
+languages.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pLanguages.innerHTML = '';
+    var items = value.split(',').map(function (item) { return item.trim(); });
+    items.forEach(function (item) {
+        if (item) { // Ensure the item is not an empty string
+            var li = document.createElement('li');
+            li.textContent = item;
+            pLanguages.appendChild(li);
+        }
+    });
+});
+var exp_1_title = document.getElementById("exp_1_title");
+var pExp_1_title = document.getElementById("p_exp_1_title");
+exp_1_title.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_1_title.textContent = value;
+});
+var exp_1_company = document.getElementById("exp_1_company");
+var pExp_1_company = document.getElementById("p_exp_1_company");
+exp_1_company.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_1_company.textContent = value;
+});
+var exp_1_role = document.getElementById("exp_1_role");
+var pExp_1_role = document.getElementById("p_exp_1_role");
+exp_1_role.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_1_role.textContent = value;
+});
+var exp_2_title = document.getElementById("exp_2_title");
+var pExp_2_title = document.getElementById("p_exp_2_title");
+exp_2_title.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_2_title.textContent = value;
+});
+var exp_2_company = document.getElementById("exp_2_company");
+var pExp_2_company = document.getElementById("p_exp_2_company");
+exp_2_company.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_2_company.textContent = value;
+});
+var exp_2_role = document.getElementById("exp_2_role");
+var pExp_2_role = document.getElementById("p_exp_2_role");
+exp_2_role.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_2_role.textContent = value;
+});
+var exp_3_title = document.getElementById("exp_3_title");
+var pExp_3_title = document.getElementById("p_exp_3_title");
+exp_3_title.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_3_title.textContent = value;
+});
+var exp_3_company = document.getElementById("exp_3_company");
+var pExp_3_company = document.getElementById("p_exp_3_company");
+exp_3_company.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_3_company.textContent = value;
+});
+var exp_3_role = document.getElementById("exp_3_role");
+var pExp_3_role = document.getElementById("p_exp_3_role");
+exp_3_role.addEventListener("input", function (event) {
+    var value = event.target.value;
+    pExp_3_role.textContent = value;
+});
--- js/index.ts
+++ js/index.ts
@@ -11,3 +11,217 @@ document.addEventListener('DOMContentLoaded', () => {
         button.textContent = isOpen ? 'Show Less' : 'Show More Experience';
     });
 });
+
+
+
+const fullname = document.getElementById("fullname") as HTMLInputElement;
+const pFullname = document.getElementById("p_fullname") as HTMLHeadingElement;
+
+fullname.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+
+    if (!value.trim()) {
+        alert("Full name is required");
+        pFullname.textContent = "";
+    } else {
+        pFullname.textContent = value;
+    }
+
+});
+
+const tagline = document.getElementById("tagline") as HTMLInputElement;
+const pTagline = document.getElementById("p_tagline") as HTMLParagraphElement;
+
+tagline.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pTagline.textContent = value;
+});
+
+const bio = document.getElementById("bio") as HTMLInputElement;
+const pbio = document.getElementById("p_bio") as HTMLParagraphElement;
+
+bio.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pbio.textContent = value;
+});
+
+const phone = document.getElementById("phone") as HTMLInputElement;
+const pPhone = document.getElementById("p_phone") as HTMLAnchorElement;
+
+phone.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pPhone.textContent = value;
+    pPhone.href = `tel:${value}`
+});
+
+const email = document.getElementById("email") as HTMLInputElement;
+const pEmail = document.getElementById("p_email") as HTMLAnchorElement;
+
+email.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pEmail.textContent = value;
+    pEmail.href = `mailto:${value}`
+});
+
+const github = document.getElementById("github_url") as HTMLInputElement;
+const pGithub = document.getElementById("p_github") as HTMLAnchorElement;
+
+github.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pGithub.textContent = value;
+    pGithub.href = `${value}`
+});
+
+const linkedin = document.getElementById("linkedin_url") as HTMLInputElement;
+const pLinkedin = document.getElementById("p_linkedin") as HTMLAnchorElement;
+
+linkedin.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pLinkedin.textContent = value;
+    pLinkedin.href = `${value}`
+});
+
+
+const education_1 = document.getElementById("education_1") as HTMLInputElement;
+const pEducation_1 = document.getElementById("p_education_title_1") as HTMLParagraphElement;
+
+education_1.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pEducation_1.textContent = value;
+});
+
+const education_1_year = document.getElementById("education_1_year") as HTMLInputElement;
+const pEducation_1_year = document.getElementById("p_education_year_1") as HTMLElement;
+
+education_1_year.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pEducation_1_year.textContent = value;
+});
+
+const education_2 = document.getElementById("education_2") as HTMLInputElement;
+const pEducation_2 = document.getElementById("p_education_title_2") as HTMLParagraphElement;
+
+education_2.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pEducation_2.textContent = value;
+});
+
+const education_2_year = document.getElementById("education_2_year") as HTMLInputElement;
+const pEducation_2_year = document.getElementById("p_education_year_2") as HTMLElement;
+
+education_2_year.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pEducation_2_year.textContent = value;
+});
+
+const skills = document.getElementById("skills") as HTMLTextAreaElement;
+const pSkills = document.getElementById("p_skills") as HTMLUListElement;
+
+skills.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pSkills.innerHTML = '';
+
+    const items = value.split(',').map(item => item.trim());
+
+    items.forEach(item => {
+        if (item) { // Ensure the item is not an empty string
+            const li = document.createElement('li');
+            li.textContent = item;
+            pSkills.appendChild(li);
+        }
+    });
+});
+
+const languages = document.getElementById("languages") as HTMLTextAreaElement;
+const pLanguages = document.getElementById("p_languages") as HTMLUListElement;
+
+languages.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pLanguages.innerHTML = '';
+
+    const items = value.split(',').map(item => item.trim());
+
+    items.forEach(item => {
+        if (item) { // Ensure the item is not an empty string
+            const li = document.createElement('li');
+            li.textContent = item;
+            pLanguages.appendChild(li);
+        }
+    });
+});
+
+
+const exp_1_title = document.getElementById("exp_1_title") as HTMLInputElement;
+const pExp_1_title = document.getElementById("p_exp_1_title") as HTMLHeadingElement;
+
+exp_1_title.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_1_title.textContent = value;
+});
+
+const exp_1_company = document.getElementById("exp_1_company") as HTMLInputElement;
+const pExp_1_company = document.getElementById("p_exp_1_company") as HTMLElement;
+
+exp_1_company.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_1_company.textContent = value;
+});
+
+const exp_1_role = document.getElementById("exp_1_role") as HTMLInputElement;
+const pExp_1_role = document.getElementById("p_exp_1_role") as HTMLParagraphElement;
+
+exp_1_role.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_1_role.textContent = value;
+});
+
+
+
+const exp_2_title = document.getElementById("exp_2_title") as HTMLInputElement;
+const pExp_2_title = document.getElementById("p_exp_2_title") as HTMLHeadingElement;
+
+exp_2_title.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_2_title.textContent = value;
+});
+
+const exp_2_company = document.getElementById("exp_2_company") as HTMLInputElement;
+const pExp_2_company = document.getElementById("p_exp_2_company") as HTMLElement;
+
+exp_2_company.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_2_company.textContent = value;
+});
+
+const exp_2_role = document.getElementById("exp_2_role") as HTMLInputElement;
+const pExp_2_role = document.getElementById("p_exp_2_role") as HTMLParagraphElement;
+
+exp_2_role.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_2_role.textContent = value;
+});
+
+
+const exp_3_title = document.getElementById("exp_3_title") as HTMLInputElement;
+const pExp_3_title = document.getElementById("p_exp_3_title") as HTMLHeadingElement;
+
+exp_3_title.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_3_title.textContent = value;
+});
+
+const exp_3_company = document.getElementById("exp_3_company") as HTMLInputElement;
+const pExp_3_company = document.getElementById("p_exp_3_company") as HTMLElement;
+
+exp_3_company.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_3_company.textContent = value;
+});
+
+const exp_3_role = document.getElementById("exp_3_role") as HTMLInputElement;
+const pExp_3_role = document.getElementById("p_exp_3_role") as HTMLParagraphElement;
+
+exp_3_role.addEventListener("input", (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    pExp_3_role.textContent = value;
+});
